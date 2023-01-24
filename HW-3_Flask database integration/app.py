a7c76ad16dce0220ba17a85bafd5d88ccb7e00b1
Nam Le <104432198+namhoangle236@users.noreply.github.com>
2023-01-24 01:55:27 -0800

    Correct commit
    remove mistake files upload, this this the correct assignment

--- HW-3_Flask database integration/app.py
+++ HW-3_Flask database integration/app.py
@@ -1,40 +1,61 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite' 
+app.config['SECRET_KEY'] = "namleislearningflask"
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False # make it so that it doesn't track, so doesn't show tracking error
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.sqlite3'
+db = SQLAlchemy(app) #Flask app connects to a db using SQLAlchemy
 
-db = SQLAlchemy()
+class Milks(db.Model):
+    id = db.Column('milk_id', db.Integer, primary_key = True)    # Auto-incrementing ID
+    name = db.Column(db.String(50))
+    flavor = db.Column(db.String(50))
+    color = db.Column(db.String(50))
+    pasteurize = db.Column(db.String(50))
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200))
-    complete = db.Column(db.Boolean)
+def __init__(self, name, flavor, color):
+   self.name = name
+   self.flavor = flavor
+   self.color = color
+   self.pasteurize = pasteurize
 
+# create / use the database
+with app.app_context():
+  db.create_all()
+
+# route to index page
 @app.route('/')
 def index():
-    incomplete = Todo.query.filter_by(complete=False).all()
-    complete = Todo.query.filter_by(complete=True).all()
+    # create a 'milks' variable to store all the Milks class's query, then pass it to index.html as 'milks'
+    return render_template('index.html', milks = Milks.query.all())
 
-    return render_template('index.html', incomplete=incomplete, complete=complete)
-
+# make add function to add new milks
 @app.route('/add', methods=['POST'])
 def add():
-    todo = Todo(text=request.form['todoitem'], complete=False)
-    db.session.add(todo)
+    # Put data received from html form in the 'Milks' class (to it's attributes), then assign to a 'milk' variable to be added to database
+    milk = Milks(name=request.form.get('name'), flavor=request.form.get('flavor'), color=request.form.get('color'), pasteurize=request.form.get('pasteurize'))
+    # Add and commit the data to the database
+    db.session.add(milk)
     db.session.commit()
-
+    # Flash a message to notify successful addition
+    flash('New milk has been added!')
+    # Redirect back to the index, but now with new database items added
     return redirect(url_for('index'))
 
-@app.route('/complete/<id>')
-def complete(id):
+# make route to detail page to display more info about milk
+@app.route('/detail')   # methods should be GET, since it's a hyperlink to the detail page
+def detail():
+    name = request.args.get('name')
+    flavor = request.args.get('flavor')
+    color = request.args.get('color')
+    pasteurize = request.args.get('pasteurize')
 
-    todo = Todo.query.filter_by(id=int(id)).first()
-    todo.complete = True
-    db.session.commit()
+    return f'Milk name: {name}, flavor: {flavor}, color: {color}, is pasteurized: {pasteurize}'
     
-    return redirect(url_for('index'))
 
-if __name__ == '__main__':
+
+    
+if __name__ == "__main__":
     app.run(debug=True)
